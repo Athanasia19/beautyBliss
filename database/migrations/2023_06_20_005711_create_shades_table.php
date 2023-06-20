11949c448068152f0324a5d74d7409d7c7e2b8b0
Athanasia19 <christinejoy.esparas@tup.edu.ph>
2023-06-20 21:41:38 +0800

    Update 2023_06_20_005711_create_shades_table.php

--- database/migrations/2023_06_20_005711_create_shades_table.php
+++ database/migrations/2023_06_20_005711_create_shades_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('color');
             $table->string('types');
+            $table->string('description');
             $table->timestamps();
         });
     }
